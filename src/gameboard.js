65bfb70e6d6f2ff460d6494d58293d68ad2a0436
Bekzat <esimhanuli13@gmail.com>
2023-08-17 16:24:51 +0600

    gameboard factory

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,5 +1,27 @@
-function GameBoard(){
+function CreateCell(row, col){
     return {
+        row, col, isShip:false, isHit:false
+    }
+}
+
+
+function GameBoard(rows, cols){
+
+    const board = [];
+    
+
+    for(let row = 0; row < rows; row++){
+        const rowArr = [];
+        for(let col = 0; col < cols; col++){
+            rowArr.push(CreateCell(row, col))
+        }
+
+        board.push(rowArr);
+    }
+    
+
+    return {
+        board,
         recieveAttack: function(){
             console.log('recieved attack');
         }
--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ const Ship = require('./ship');
 const Player = require('./player');
 const GameBoard = require('./gameboard');
 
+let gameboard = GameBoard(5,5);
+
+console.log(gameboard.board);
